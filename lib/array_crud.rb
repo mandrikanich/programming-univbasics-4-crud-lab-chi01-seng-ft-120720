8cf777dfb207d140f3d85b3ee5dd72f3adb6995a
Michael Andrikanich <mandrikanich@gmail.com>
2020-10-26 16:10:13 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,39 +1,33 @@
 def create_an_empty_array
-  
+[]
+ 
 end
 
 def create_an_array
+ favorite_colors = ["red", "blue", "green", "yellow"]
   
 end
 
-def add_element_to_end_of_array(array, element)
+def add_element_to_end_of_array(favorite_colors, element)
+  favorite_colors << element
+  p favorite_colors
   
 end
 
-def add_element_to_start_of_array(array, element)
+def add_element_to_start_of_array(favorite_colors, element)
+  favorite_colors.unshift (element)
+  p favorite_colors
   
 end
 
-def remove_element_from_end_of_array(array)
+def remove_element_from_end_of_array(favorite_colors)
+  favorite_colors.pop
   
 end
 
-def remove_element_from_start_of_array(array)
+def remove_element_from_start_of_array(element)
+  favorite_colors.unshift
   
 end
 
-def retrieve_element_from_index(array, index_number)
-  
-end
 
-def retrieve_first_element_from_array(array)
-  
-end
-
-def retrieve_last_element_from_array(array)
-  
-end
-
-def update_element_from_index(array, index_number, element)
-
-end
